72c5a65448329bc3f6bb362f293ce9610a5667fa
mmalektsg <139792457+mmalektsg@users.noreply.github.com>
2023-12-27 10:42:57 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// Create web server
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  // Get comments for specific post
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  // Create comment
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
